ce48ae52cbf13de740b3981dc18c0cb3e676a755
Enas <enaselmahdwi@gmail.com>
2021-12-10 05:32:26 -0700

    add dedependencies build env,server index file

--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
 // build your server here and require it from index.js
+const express = require('express');
+const projectRouter = require('./project/router');
+const resourceRouter = require('./resource/router');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/project', projectRouter);
+server.use('/api/resource', resourceRouter);
+
+server.use('*', (req, res) => {
+    // catch all 404 errors middleware
+    res.status(404).json({ message: ` not found!` });
+  });
+
+module.exports = server;
