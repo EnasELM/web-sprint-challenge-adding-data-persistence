604694c91087c12f4edd31b3e5864fe63a1ebdf8
Enas <enaselmahdwi@gmail.com>
2021-12-10 12:32:56 -0700

    up

--- api/task/router.js
+++ api/task/router.js
@@ -21,8 +21,7 @@ router.post("/", (req, res, next) => {
     .catch(next);
 });
 
-router.use((err, req, res, next) => {
-  // eslint-disable-line
+router.use((err, req, res, next) => {// eslint-disable-line
   res.status(err.status || 500).json({
     sageAdvice: "Finding the real error is 90% of the bug fix",
     message: err.message,
